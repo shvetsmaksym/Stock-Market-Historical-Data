30d0ac1d5a00e7c009cf338f24a3f97da347ff9d
Maksym Shvets <maksym.shvets@unilodz.eu>
2020-10-02 19:37:22 +0200

    stock name and date range

--- trading_system.py
+++ trading_system.py
@@ -1,7 +1,98 @@
 import yfinance as yf
+from tkinter import *
+from tkcalendar import *
+from datetime import datetime
 
-apple = yf.Ticker("AAPL")
-print(apple)
-#print(apple.info)
-hist = apple.history(start="2020-09-01", end="2020-10-01")
-print(hist)
+class Stock_data:
+
+	def __init__(self):
+		'''set main window'''
+		self.main_window = Tk()
+		self.main_window.title('Stock History Data')
+		self.main_window.geometry()
+
+		'''set labels and buttons to input parametres'''
+		#Company name
+		self.smbl_label = Label(self.main_window, text="Company name")
+		self.smbl_label.grid(row=0, column=0)
+		self.smbl_box = Entry(self.main_window, width=30)
+		self.smbl_box.grid(row=0, column=1, columnspan=2, pady=5, padx=5)
+
+		#Date range
+		self.trange_label = Label(self.main_window, text="Set the date range")
+		self.trange_label.grid(row=1, column=0)
+
+		#From date / to date variables
+		self.fd = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
+		self.td = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
+
+		#From date / to date buttons and labels
+		self.from_date = Button(self.main_window, text="from", width=10, command=self.set_from)
+		self.from_date.grid(row=1, column=1)
+		self.fd_label = Label(self.main_window, text="")
+		self.fd_label.grid(row=2, column=1, pady=5)
+
+		self.to_date = Button(self.main_window, text="to", width=10, command=self.set_to)
+		self.to_date.grid(row=1, column=2)
+		self.td_label = Label(self.main_window, text="")
+		self.td_label.grid(row=2, column=2, pady=5)
+
+		#Submit parameters
+		self.submit_button = Button(self.main_window, text="Submit", command=self.download_data)
+		self.submit_button.grid(row=3, column=1, pady=5)
+
+
+	def run(self):
+		self.main_window.mainloop()
+
+
+	def download_data(self):
+		stock = yf.Ticker(self.smbl_box.get())
+		history = stock.history(start=self.fd, end=self.td)
+
+		hist_window = Tk()
+		hist_window.title(stock.info["longName"])
+
+		hist_table = Label(hist_window, text=history)
+		hist_table.grid(row=0, column=0)
+
+		hist_window.mainloop()
+	
+
+	def set_date(conf):
+
+		def wrapper(self):
+
+			self.cal = Tk()
+			self.cal.title("")
+
+			today = datetime.now()
+			self.calendar = Calendar(self.cal, date_pattern='y-mm-dd', selectmode="day", year=today.year, month=today.month, day=today.day)
+			self.calendar.pack()
+
+			confirm_button = Button(self.cal, text="Confirm", command = lambda: conf(self))
+			confirm_button.pack()
+
+			self.cal.mainloop()
+
+		return wrapper
+
+	@set_date
+	def set_from(self):
+
+		self.fd = self.calendar.get_date()
+		self.fd_label = Label(self.main_window, text=self.calendar.get_date())
+		self.fd_label.grid(row=2, column=1, pady=10)
+		self.cal.destroy()
+
+	@set_date
+	def set_to(self):
+
+		self.td = self.calendar.get_date()
+		self.td_label = Label(self.main_window, text=self.calendar.get_date())
+		self.td_label.grid(row=2, column=2, pady=10)
+		self.cal.destroy()
+
+if __name__ == "__main__":
+	s = Stock_data()
+	s.run()
