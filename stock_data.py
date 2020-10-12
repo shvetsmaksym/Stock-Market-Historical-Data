63e1735cc1910832b08a30228579224e6229be6b
Maksym Shvets <maksym.shvets@unilodz.eu>
2020-10-12 18:33:25 +0200

    add show chart option

--- stock_data.py
+++ stock_data.py
@@ -3,76 +3,80 @@ from tkinter import *
 from tkcalendar import *
 from datetime import datetime
 from pandastable import Table
+import mplfinance as mpf
 
 class Stock_data:
 
 	def __init__(self):
 		'''set main window'''
-		self.main_window = Tk()
-		self.main_window.title('Stock History Data')
-		self.main_window.geometry()
+		self.mainWindow = Tk()
+		self.mainWindow.title('Stock History Data')
+		self.mainWindow.geometry()
 
 		'''set labels and buttons to input parametres'''
 		#Company name
-		self.smbl_label = Label(self.main_window, text="Company name")
-		self.smbl_label.grid(row=0, column=0)
-		self.smbl_box = Entry(self.main_window, width=30)
-		self.smbl_box.grid(row=0, column=1, columnspan=2, pady=5, padx=5)
+		self.smblLabel = Label(self.mainWindow, text="Company name")
+		self.smblLabel.grid(row=0, column=0)
+		self.smblBox = Entry(self.mainWindow, width=30)
+		self.smblBox.grid(row=0, column=1, columnspan=2, pady=5, padx=5)
 
 		#Date range
-		self.trange_label = Label(self.main_window, text="Set the date range")
-		self.trange_label.grid(row=1, column=0)
+		self.timerangeLabel = Label(self.mainWindow, text="Set the date range")
+		self.timerangeLabel.grid(row=1, column=0)
 
-		#From date / to date variables
-		self.fd = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
-		self.td = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
+		#From date / to date variables (today is default)
+		self.fromDate = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
+		self.toDate = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(datetime.now().day)
 
 		#From date / to date buttons and labels
-		self.from_date = Button(self.main_window, text="from", width=10, command=self.set_from)
-		self.from_date.grid(row=1, column=1)
-		self.fd_label = Label(self.main_window, text="")
-		self.fd_label.grid(row=2, column=1, pady=5)
+		self.fromDateButton = Button(self.mainWindow, text="from", width=10, command=self.set_from)
+		self.fromDateButton.grid(row=1, column=1)
+		self.fromDateLabel = Label(self.mainWindow, text="")
+		self.fromDateLabel.grid(row=2, column=1, pady=5)
 
-		self.to_date = Button(self.main_window, text="to", width=10, command=self.set_to)
-		self.to_date.grid(row=1, column=2)
-		self.td_label = Label(self.main_window, text="")
-		self.td_label.grid(row=2, column=2, pady=5)
+		self.toDateButton = Button(self.mainWindow, text="to", width=10, command=self.set_to)
+		self.toDateButton.grid(row=1, column=2)
+		self.toDateLabel = Label(self.mainWindow, text="")
+		self.toDateLabel.grid(row=2, column=2, pady=5)
 
 		#Submit parameters
-		self.submit_button = Button(self.main_window, text="Submit", command=self.download_data)
-		self.submit_button.grid(row=3, column=1, pady=5)
+		self.submitButton = Button(self.mainWindow, text="Submit", command=self.download_data)
+		self.submitButton.grid(row=3, column=1, pady=5)
 
 
 	def run(self):
-		self.main_window.mainloop()
+		self.mainWindow.mainloop()
 
 
 	def download_data(self):
 		# getting stock data
-		stock = yf.Ticker(self.smbl_box.get())
-		hist = stock.history(start=self.fd, end=self.td)
+		stock = yf.Ticker(self.smblBox.get())
+		hist = stock.history(start=self.fromDate, end=self.toDate)
+		hist["Date"] = hist.index
 
 		# create new window
-		hist_window = Tk()
-		hist_window.title(self.smbl_box.get())
+		histWindow = Tk()
+		histWindow.title(self.smblBox.get())
 
-		frame = Frame(hist_window)
+		frame = Frame(histWindow)
 		frame.pack()
 
 		# create table (pandastable.Table(tk.frame))
-		table = Table(frame)
-		# write stock data to the table
-		table.model.df = hist
-		# update changes in table
-		table.redraw()
+		table = Table(frame, dataframe=hist[["Date", "Open", "High", "Low", "Close"]])
 		table.show()
+		
+		chartButton = Button(histWindow, text="Show chart", command = lambda: self.show_chart(hist)).pack()
 
-		chart_button = Button(hist_window, text="Show chart").pack()
-
-		hist_window.mainloop()
+		histWindow.mainloop()
 	
+	
+	def show_chart(self, df):
+		"""Shows stock data as candleistick chart"""
+		mpf.plot(df, type="candle")
+
 
 	def set_date(conf):
+		"""Opens new window with calendar"""
 
 		def wrapper(self):
 
@@ -83,8 +87,8 @@ class Stock_data:
 			self.calendar = Calendar(self.cal, date_pattern='y-mm-dd', selectmode="day", year=today.year, month=today.month, day=today.day)
 			self.calendar.pack()
 
-			confirm_button = Button(self.cal, text="Confirm", command = lambda: conf(self))
-			confirm_button.pack()
+			confirmButton = Button(self.cal, text="Confirm", command = lambda: conf(self))
+			confirmButton.pack()
 
 			self.cal.mainloop()
 
@@ -92,18 +96,20 @@ class Stock_data:
 
 	@set_date
 	def set_from(self):
+		"""gets set in calendar window date"""
 
-		self.fd = self.calendar.get_date()
-		self.fd_label = Label(self.main_window, text=self.calendar.get_date())
-		self.fd_label.grid(row=2, column=1, pady=10)
+		self.fromDate = self.calendar.get_date()
+		self.fromDateLabel = Label(self.mainWindow, text=self.calendar.get_date())
+		self.fromDateLabel.grid(row=2, column=1, pady=10)
 		self.cal.destroy()
 
 	@set_date
 	def set_to(self):
+		"""gets set in calendar window date"""
 
-		self.td = self.calendar.get_date()
-		self.td_label = Label(self.main_window, text=self.calendar.get_date())
-		self.td_label.grid(row=2, column=2, pady=10)
+		self.toDate = self.calendar.get_date()
+		self.toDateLabel = Label(self.mainWindow, text=self.calendar.get_date())
+		self.toDateLabel.grid(row=2, column=2, pady=10)
 		self.cal.destroy()
 
 if __name__ == "__main__":
